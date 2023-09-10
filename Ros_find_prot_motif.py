4cfe6baab025559fe79f11c33e82ea151e91d372
sviatkh <sviatoslavkharuk@gmail.com>
2023-09-10 17:14:29 +0300

    function for dictionary create

--- Ros_find_prot_motif.py
+++ Ros_find_prot_motif.py
@@ -8,9 +8,9 @@ def main():
     # ask for the id numbers
     id_list = input("Enter a Id number: ").strip().split()
     # assign a key and value into the new variables
-    key, value = get_seq(id_list)
-    print(key, value)
+    key_list, value_list = get_seq(id_list)
     # using the function and key and values create a dictionary 
+    make_dictionary(key_list, value_list)
 
 def get_seq(id_list): #function for get the id-s sequences 
     Entrez.email = "sviatoslavkharuk@gmail.com" 
@@ -22,27 +22,16 @@ def get_seq(id_list): #function for get the id-s sequences
             key = [aa_record.id for aa_record in aa_records]
             value = [str(aa_record.seq) for aa_record in aa_records]
             return key, value
+
 ## loop for parsing and saving the sequences in dictionary
-# def make_dictionary(key, value):
-    
+id_dictionary = {}
+def make_dictionary(key_list, value_list):
+     for key in key_list:
+          for value in value_list:
+               id_dictionary[key] = value
 
-
-
-
-
-#     # save the sequence in the file for further work
-#     with open("pro_seq.txt", "w+") as pro_seq: ## зберегти як словник? айді як ключ, а послідовність як значення 
-#         pro_seq.write(seq_aa)
-
-# ## maybe I do not need to have this and just have a one dictionary 
-# # open file and read  
-# with open("pro_seq.txt", "r") as protein:
-#     reader = protein.read()
-#     print(reader)
-
-
-# ## rebuild on work with dictionary
-# # re.expressions
+# ## rebuild to work with dictionary
+# # re.expressions for searching the protein motif
 # pattern = r"N[^P][ST]"
 
 # ## loop for matches in the values
